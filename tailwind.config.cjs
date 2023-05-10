1afd13151c1460beb55dd7ba55b3c61d28a0fe64
roopsBee <35971830+roopsBee@users.noreply.github.com>
2023-05-10 17:20:04 +0100

    style:add clamp padding

--- tailwind.config.cjs
+++ tailwind.config.cjs
@@ -2,7 +2,12 @@
 module.exports = {
   content: ["./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}"],
   theme: {
-    extend: {},
+    extend: {
+      padding: {
+        fluidSm: "clamp(0rem, 3vw, 1.5rem)",
+        fluid: "clamp(1rem, 7vw, 3rem)",
+      },
+    },
   },
   plugins: [require("@tailwindcss/typography")],
 };
