4568c8f6f4de8a66f59a2d681bf21cb3e27653f5
roopsBee <35971830+roopsBee@users.noreply.github.com>
2023-12-05 16:11:19 +0000

    update packages

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,18 +1,17 @@
 import { defineConfig } from 'astro/config';
 import mdx from '@astrojs/mdx';
 import sitemap from '@astrojs/sitemap';
-import vercel from '@astrojs/vercel/serverless';
 import tailwind from '@astrojs/tailwind';
-
+import vercel from '@astrojs/vercel/serverless';
 
 // https://astro.build/config
 export default defineConfig({
-  site: 'https://example.com',
-  integrations: [mdx(), sitemap(), tailwind()],
-  adapter:vercel({
-    webAnalytics: {
-      enabled: true,
-    },
-  }),
-  output: "server"
+	site: 'https://beestack.dev',
+	integrations: [mdx(), sitemap(), tailwind()],
+	adapter: vercel({
+		webAnalytics: {
+			enabled: true,
+		},
+	}),
+	output: 'server',
 });
